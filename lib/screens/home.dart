c2e96ea76e3e816807ed4487b32692eba75c6d0c
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-04 14:12:53 -0600

    building out the themes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,15 @@
 import "package:flutter/material.dart";
 
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).padding.top;
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(top: size),
+      ),
+    ));
   }
 }
